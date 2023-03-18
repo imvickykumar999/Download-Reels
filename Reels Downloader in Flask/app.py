a29f524f2ae6a08ac1528590ee188f36b32ff2fa
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-18 14:55:16 +0530

    Update app.py

--- Reels Downloader in Flask/app.py
+++ Reels Downloader in Flask/app.py
@@ -11,6 +11,7 @@ def getfollowedby(url):
     user = requests.get(link)
 
     a = user.json()
+    # print(a)
     b = a['graphql']['user']['edge_followed_by']['count']
     
     c = a['graphql']['user']['edge_felix_video_timeline']['edges']
