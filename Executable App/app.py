0665b980d3f9f4a15b5cbc92797965ba9be083cd
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-31 19:45:49 +0530

    exe in dist ss
    Created executable file to download reels, input via url parameter id = C....

---
+++ Executable App/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, redirect
+import requests
+
+app = Flask(__name__)
+@app.route('/') 
+def home():
+    try:
+        return redirect(requests.get(f'https://www.instagram.com/p/{request.args.get("id")}/?__a=1&__d=1').json()['graphql']['shortcode_media']['video_url'], code=200)
+    except:
+        return 'Replace id parameter with your REEL_ID : <a href="http://127.0.0.1:5000/?id=ClwrpW1BB-R">id = ClwrpW1BB-R</a>'
+
+if __name__ =='__main__':  
+    app.run(debug = True)
--- Reels Downloader in Flask/app.py
+++ Reels Downloader in Flask/app.py
@@ -1,5 +1,7 @@
 """Flask Login Example and instagram fallowing find"""
 
+# import requests;print(requests.get('https://www.instagram.com/p/CqTC9d3vYlX/?__a=1&__d=1').json()['graphql']['shortcode_media']['edge_sidecar_to_children']['edges'][0]['node']['video_url'])
+
 from flask import Flask, render_template, request
 import requests
 
