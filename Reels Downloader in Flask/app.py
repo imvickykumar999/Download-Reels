75c171ac1c532c177f50adcfbbd59b37bab496af
imvickykumar999 <imvickykumar999@gmail.com>
2023-04-06 16:33:01 +0530

    made default public id

--- Reels Downloader in Flask/app.py
+++ Reels Downloader in Flask/app.py
@@ -98,8 +98,11 @@ def home():
 
 
 if __name__ == '__main__':
-    app.secret_key = "123"
-    app.run(debug=True)
+    # run app in debug mode on port 5000
+    app.run(debug=True, 
+            port=5000, 
+            host='0.0.0.0',
+            )
 
 # -----------------------
 
