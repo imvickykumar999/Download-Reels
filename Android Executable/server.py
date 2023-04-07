7747523924d634c674693e77ff086a295ee41f76
imvickykumar999 <imvickykumar999@gmail.com>
2023-04-07 10:19:00 +0530

    server.exe

---
+++ Android Executable/server.py
@@ -0,0 +1,19 @@
+
+import requests, zipfile, os
+URL = 'https://github.com/imvickykumar999/Download-Reels/releases/download/Public_IP/android.zip'
+
+r = requests.get(URL, allow_redirects=True)
+open('android.zip', 'wb').write(r.content)
+
+with zipfile.ZipFile('android.zip', 'r') as zip_ref:
+    zip_ref.extractall('.')
+
+print('''
+
+Public Server started at
+http://192.168.0.103:5000
+
+Access this link from any Device nearby.
+
+''')
+os.system('python app.py')
